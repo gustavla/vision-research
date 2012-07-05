ed3cb0dfd01aa2a26d93c6cac2acba93717b608e
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-07-05 09:33:09 -0500

    Mid-way changing format of u in imagedef.

--- v1/gimagedef.py
+++ v1/gimagedef.py
@@ -3,15 +3,18 @@ import numpy as np
 from scipy import interpolate
 from scipy import linalg
 import amitgroup as ag
-from amitgroup.ml.imagedefw import imagedef, deform, deform_map
+from amitgroup.ml.imagedefw import imagedef, deform, deform__new, deform_map, deform_map__new
 from copy import copy
 from math import cos 
 from itertools import product
 import pywt
 PLOT = True 
-#PLOT = False
+PLOT = False
 if PLOT: 
     import matplotlib.pylab as plt
+
+#TEST = False
+TEST = True
 
 def main():
     #import amitgroup.io.mnist
@@ -21,6 +24,10 @@ def main():
     shifted[:-3,:] = images[0,3:,:]
 
     im1, im2 = np.zeros((32, 32)), np.zeros((32, 32))
+
+    wl_name = "db2"
+    levels = len(pywt.wavedec(range(32), wl_name)) - 1
+    scriptNs = map(len, pywt.wavedec(range(32), wl_name, level=levels)) + [0]
 
     #im1, im2 = images[0], shifted
     im1[:28,:28] = images[0]
@@ -49,8 +56,8 @@ def main():
     #im1 = im1[:,::-1]
     #im2 = im2[:,::-1]
 
-    levels = 3
-    scriptNs = map(len, pywt.wavedec(range(32), 'db2', level=levels)) + [0]
+    #levels = 3
+    #scriptNs = map(len, pywt.wavedec(range(32), 'db2', level=levels)) + [0]
 
     minA = 2
     A = 2 
@@ -101,9 +108,9 @@ def main():
             for x1 in range(xs.shape[1]):
                 xs[x0,x1] = np.array([x0/float(xs.shape[0]), x1/float(xs.shape[1])])
         
-        defx = deform_map(xs, u)
+        defx = deform_map(xs, u, scriptNs)
 
-        imdef = deform(im1, u)
+        imdef = deform(im1, u, scriptNs)
         d = dict(origin='lower', interpolation='nearest', cmap=plt.cm.gray)
 
         plt.figure(figsize=(14,4))
@@ -127,14 +134,14 @@ def main():
         im1b = im1#ag.math.blur_image(im1, 2)
         im2b = im2#ag.math.blur_image(im2, 2)
 
-        u, costs, logpriors, loglikelihoods = imagedef(im1b, im2b, A=A)
+        u, costs, logpriors, loglikelihoods, u__new = imagedef(im1b, im2b, A=A)
         #print "Final"
         #print u[0][0][0,0], 0.0
         #print u[0][0][1,0], 0.0
         #print u[0][0][2,0], initial 
 
         if PLOT and costs:
-            plotfunc = plt.semilogy
+            plotfunc = plt.plot#plt.semilogy
             plt.figure(figsize=(8,12))
             plt.subplot(211)
             plotfunc(costs, label="J")
@@ -146,9 +153,10 @@ def main():
             plt.show()
 
     
+        im3 = deform(im1, u, scriptNs)
+        im3__new = deform__new(im1, u__new, scriptNs)
         if PLOT:
             d = dict(origin='lower', interpolation='nearest', cmap=plt.cm.gray)
-            im3 = deform(im1, u)
 
             plt.figure(figsize=(16,6))
             plt.subplot(141)
@@ -175,6 +183,19 @@ def main():
         plt.title("I")
         plt.imshow(im2, origin='lower') 
         plt.show()
+
+
+    import pickle
+    if TEST:
+        im3correct = pickle.load(open('im3.p', 'rb'))
+        passed = (im3__new == im3correct).all()
+        print "PASSED__NEW:", ['NO', 'YES'][passed] 
+        print ((im3__new - im3correct)**2).sum()
+        passed = (im3 == im3correct).all()
+        print "PASSED:", ['NO', 'YES'][passed] 
+        print ((im3 - im3correct)**2).sum()
+    else:
+        pickle.dump(im3, open('im3.p', 'wb'))
     
 
 if __name__ == '__main__':
