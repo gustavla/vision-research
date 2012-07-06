e0faab93c848e20f7ea5cc1cfec29a3de44ddf2c
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-07-06 00:18:01 -0500

    Migrating to ImageDeformation.

--- v1/gimagedef.py
+++ v1/gimagedef.py
@@ -39,51 +39,15 @@ def main():
     im2 = im2[::-1,:]
 
     A = 2 
-    if 0:
-        u = []
-        for q in range(2):
-            u0 = [np.zeros((1,1))]
-            for s in range(0, A):
-                u0.append((np.zeros((2**s,2**s)), np.zeros((2**s,2**s)), np.zeros((2**s,2**s))))
-            u.append(u0)
-
-        #u[0][1][0][0] += 0.5 
-        #u[1][1][1][0] += 0.2 
         
-        #u[0][1:][2][0][0,3] = 0.2 
-
-        #print u[0][1:][2][0]
-
-        xs = np.empty((20, 20, 2))
-        for x0 in range(xs.shape[0]):
-            for x1 in range(xs.shape[1]):
-                xs[x0,x1] = np.array([x0/float(xs.shape[0]), x1/float(xs.shape[1])])
-        
-        defx = ag.ml.deform_map(xs, u, scriptNs)
-
-        imdef = deform(im1, u, scriptNs)
-        d = dict(origin='lower', interpolation='nearest', cmap=plt.cm.gray)
-
-        plt.figure(figsize=(14,4))
-        plt.subplot(131)
-        plt.title("Original")
-        plt.imshow(im1, **d) 
-        plt.subplot(132)
-        plt.title("Deformed")
-        plt.imshow(imdef, **d)
-        plt.subplot(133)
-        plt.title("Deform map")
-        plt.quiver(xs[:,:,1], xs[:,:,0], defx[:,:,1], defx[:,:,0])
-        plt.show()
-        
-    elif 1:
+    if 1:
         # Blur images
         im1b = im1#ag.math.blur_image(im1, 2)
         im2b = im2#ag.math.blur_image(im2, 2)
 
         show_costs = True
 
-        u, info = ag.ml.imagedef(im1b, im2b, rho=3.0, calc_costs=show_costs)
+        imgdef, info = ag.ml.imagedef(im1b, im2b, rho=3.0, calc_costs=show_costs)
 
 
         if PLOT and show_costs:
@@ -104,8 +68,7 @@ def main():
             plt.show()
 
     
-        if u is not None:
-            im3 = ag.ml.deform(im1, u, scriptNs)
+        im3 = imgdef.deform(im1)
 
         if PLOT:
             d = dict(origin='lower', interpolation='nearest', cmap=plt.cm.gray)
@@ -140,10 +103,9 @@ def main():
     import pickle
     if TEST:
         im3correct = pickle.load(open('im3.p', 'rb'))
-        if u is not None:
-            passed = (im3 == im3correct).all()
-            print "PASSED:", ['NO', 'YES'][passed] 
-            print ((im3 - im3correct)**2).sum()
+        passed = (im3 == im3correct).all()
+        print "PASSED:", ['NO', 'YES'][passed] 
+        print ((im3 - im3correct)**2).sum()
     else:
         pickle.dump(im3, open('im3.p', 'wb'))
     
