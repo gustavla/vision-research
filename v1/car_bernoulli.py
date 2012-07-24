d90331c503c44910388f55a114b3fbe0b3bdadc1
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-07-24 08:46:54 -0500

    Added Sketchup generator.

--- v1/car_bernoulli.py
+++ v1/car_bernoulli.py
@@ -2,22 +2,31 @@ from __future__ import print_function
 import numpy as np
 import amitgroup as ag
 import amitgroup.stats
+import amitgroup.features
 import matplotlib.pylab as plt
+import sys
 
 # generate synthetic data
 
-XS = np.load("test.npz")['data']
+XS_ = np.load("car.npy")
 
-num_mixtures = 18 
+edges = ag.features.bedges(XS_, 5)
+print(edges.shape)
+edges = np.rollaxis(edges, 3)
+print(edges.shape)
+XS = edges[int(sys.argv[1])]
+
+sh = 3, 3
+num_mixtures = np.prod(sh)
 
 bm = ag.stats.BernoulliMixture(num_mixtures, XS)
 bm.run_EM(.0001)
 
 np.savez('output.npz', weights=bm.weights, templates=bm.templates)
 
-plt.figure(figsize=(14,5))
-for i in range(num_mixtures)[:3*9]:
-    plt.subplot(3, 6, 1+i)
+plt.figure(figsize=(8,8))
+for i in range(num_mixtures)[:np.prod(sh)]:
+    plt.subplot(sh[0], sh[1], 1+i).set_axis_off()
     plt.imshow(bm.templates[i], interpolation='nearest', cmap=plt.cm.gray)
 
 print(XS)
