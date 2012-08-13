9f695caa5502d3768bd483d490ca3802d441df18
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-08-12 22:56:55 -0500

    Updated train_def_coefs.py.

--- mnist/train_def_coefs.py
+++ mnist/train_def_coefs.py
@@ -29,28 +29,25 @@ mixtures_file = np.load(mixtures_filename)
 all_templates = mixtures_file['templates']
 all_affinities = mixtures_file['affinities']
 
-#if PLOT:
-#    plw = ag.plot.PlottingWindow(figsize=(5, 9), subplots=(8, 3))
-
 M = all_affinities.shape[1]
 
 if digit is not None:
     digits = [digit]
     shape = (1, M)
+    d0 = digit
 else:
     digits = range(10)
     shape = (10, M)
+    d0 = 0
 
 sh = shape + ag.util.DisplacementFieldWavelet.shape_for_size(all_templates.shape[2:4])
 means = np.empty(sh)
 variances = np.empty(sh)
 
-llh_sh = (10, M)
+llh_sh = shape 
 llh_means = np.empty(llh_sh)
 llh_variances = np.empty(llh_sh)
 
-#means = [[] for i in range(M)]
-#variances = [[] for i in range(M)]
 for d in digits:
     entries = [[] for i in range(M)]
     slices = [[] for i in range(M)]
@@ -61,33 +58,30 @@ for d in digits:
         n0 = inspect
         n1 = n0+1 
 
+    n1 = 30 
+
     us = []
     for i in range(n0, n1):
-        #if PLOT and not plw.tick():
-        #    sys.exit(0) 
         affinities = all_affinities[i]
         m = np.argmax(affinities)
         F = np.rollaxis(all_templates[d,m], axis=2)
         I = np.rollaxis(all_features[i], axis=2).astype(float)
 
-        t1 = time.time()
-        imdef, info = ag.stats.bernoulli_deformation(F, I, penalty=100.0, gtol=0.1, maxiter=10, last_level=3, wavelet='db8', debug_plot=PLOT)
-        t2 = time.time()
-        print i, "time:", (t2-t1)
-        #imdef, info = ag.ml.bernoulli_deformation(F, I, penalty=100.0, stepsize_scale_factor=0.1, tol=0.000001, maxiter=1000, last_level=4, wavelet='db2', debug_plot=False)
+        x, y = ag.util.DisplacementFieldWavelet.meshgrid_for_shape(F.shape[1:])
 
-        #us.append(imdef.u)
+        t1 = time.time()
+        imdef, info = ag.stats.bernoulli_deformation(F, I, penalty=10000.0, rho=2.0, gtol=0.1, maxiter=10, start_level=1, last_level=1, wavelet='db4', debug_plot=PLOT)
+        t2 = time.time()
+        print "{0}.{1} (time = {2}".format(d, i, t2-t1)
+
+        if imdef is None:
+            sys.exit(0) 
+
         entries[m].append(imdef.u)
         Fdef = np.asarray([
             imdef.deform(F[j]) for j in range(8)
         ])
         slices[m].append(Fdef - I)
-        
-        #if PLOT:
-        #    for j in range(8):
-        #        plw.imshow(I[j], subplot=j*3)
-        #        plw.imshow(F[j], subplot=j*3+1)
-        #        plw.imshow(imdef.deform(F[j]), subplot=j*3+2)
 
     for m in range(M):
         data = np.asarray(entries[m])
@@ -95,18 +89,15 @@ for d in digits:
 
         print means.shape, data.shape
         # Prior
-        means[d, m] = data.mean(axis=0) 
-        variances[d, m] = data.var(axis=0) 
+        print d, m, means.shape
+        means[d-d0, m] = data.mean(axis=0) 
+        variances[d-d0, m] = data.var(axis=0) 
 
         # Likelihood
         values = np.asarray(slices[m]).flatten()
-        np.save('tmp-values.{0}.npy'.format(m), values)
+        np.save("tmp-values.{0}.npy".format(m), values)
 
-        llh_means[d, m] = values.mean()
-        llh_variances[d, m] = values.var()
+        llh_means[d-d0, m] = values.mean()
+        llh_variances[d-d0, m] = values.var()
          
-    #us = np.asarray(us)
-    #means.append(us.mean())
-    #variances.append(us.var())
-
 np.savez(output_filename, prior_mean=means, prior_var=variances, llh_mean=llh_means, llh_var=llh_variances)
