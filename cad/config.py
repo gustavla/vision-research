95f9577c8462ce8ec2daac7198ea298fa59daa70
Gustav Larsson <gustav.m.larsson@gmail.com>
2012-10-26 12:55:30 -0500

    Updates to config and annotation template.

--- cad/config.py
+++ cad/config.py
@@ -3,11 +3,22 @@ import os.path
 def rel(x):
     return os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
 
-SETTINGS = dict( 
-    src_dir='/Users/slimgee/git/data/bike',
-    dst_dir='/Users/slimgee/git/data/afew',
-    img_output_dir='/Users/slimgee/git/data/output/images',
-    anno_output_dir='/Users/slimgee/git/data/output/annotations',
-    xml_template=rel('template.xml'),
-    index_file='/Users/slimgee/git/data/output/traingen.txt'
-)
+
+if 1:
+    SETTINGS = dict( 
+        src_dir='/var/tmp/matlab/bike',
+        dst_dir='/var/tmp/matlab/VOC2007/VOCdevkit/VOC2007/JPEGImages',
+        img_output_dir='/var/tmp/matlab/VOC2007/VOCdevkit/VOC2007/JPEGImages',
+        anno_output_dir='/var/tmp/matlab/VOC2007/VOCdevkit/VOC2007/Annotations',
+        xml_template=rel('template.xml'),
+        index_file='/var/tmp/matlab/VOC2007/VOCdevkit/VOC2007/ImageSets/Main/traingen.txt'
+    )
+else: 
+    SETTINGS = dict( 
+        src_dir='/Users/slimgee/git/data/bike',
+        dst_dir='/Users/slimgee/git/data/afew',
+        img_output_dir='/Users/slimgee/git/data/output/images',
+        anno_output_dir='/Users/slimgee/git/data/output/annotations',
+        xml_template=rel('template.xml'),
+        index_file='/Users/slimgee/git/data/output/traingen.txt'
+    )
