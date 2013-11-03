5bb4d2c8428734dbef09a52a1b6a9c32809c5b80
Gustav Larsson <gustav.m.larsson@gmail.com>
2013-11-03 10:34:58 -0600

    Added numpy include dirs in setup.py (fixes cython build).

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from Cython.Distutils import extension
+import numpy as np
 
 setup(
     name = "gv",
+    # You were here!!
     ext_modules = cythonize('gv/fast.pyx'), # accepts a glob pattern
+    include_dirs = [np.get_include()],
 )
