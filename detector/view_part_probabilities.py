c2247c9bb8a67c4211776166f0afd9af1ca31b96
Gustav Larsson <gustav.m.larsson@gmail.com>
2013-12-18 11:55:54 -0600

    Added better plotting support

---
+++ detector/view_part_probabilities.py
@@ -0,0 +1,50 @@
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Train mixture model on edge data')
+parser.add_argument('parts', metavar='<parts file>', type=argparse.FileType('rb'), help='Filename of parts file')
+parser.add_argument('-o', '--output', type=argparse.FileType('wb'), help='Filename of output image')
+
+args = parser.parse_args()
+parts_file = args.parts
+
+
+import numpy as np
+import matplotlib.pylab as plt
+import amitgroup as ag
+import amitgroup.features
+import sys
+import gv
+
+#parts_dictionary = gv.PatchDictionary.load(part_file)
+descriptor = gv.BinaryDescriptor.getclass('parts').load(parts_file)
+
+originals = descriptor.visparts
+parts = descriptor.parts
+E = parts.shape[-1]
+F = descriptor.num_parts
+
+#F = 50
+
+plt.figure(figsize=(10, 10))
+shape = (F, E + 1)
+
+grid = gv.plot.ImageGrid(shape[0], shape[1], parts.shape[1:3], border_color=(0.3, 0, 0))
+
+strides = descriptor.parts.shape[0] // F
+
+for f in xrange(F):
+    #plt.subplot(*(shape + (1 + shape[1] * f,)))
+    #plt.imshow(descriptor.visparts[f], interpolation='nearest', cmap=plt.cm.gray)
+    grid.set_image(descriptor.visparts[f], f, 0, cmap=plt.cm.gray) 
+
+    for e in xrange(E):
+        #plt.subplot(*(shape + (1 + shape[1] * f + e,)))
+        #plt.imshow(descriptor.parts[f,...,e], vmin=0, vmax=1, interpolation='nearest', cmap=plt.cm.RdBu_r)
+        grid.set_image(descriptor.parts[strides*f,...,e], f, 1+e, vmin=0, vmax=1, cmap=plt.cm.RdBu_r)
+
+grid.save(args.output)
+#ag.plot.images(originals, zero_to_one=False)
+#plt.show()
+
+#ag.plot.images(np.rollaxis(parts[9], axis=2))
--- gv/plot.py
+++ gv/plot.py
@@ -1,22 +1,25 @@
 from __future__ import division
 import numpy as np
+import gv
 #from scipy.ndimage.interpolation import zoom
 
-class PlotImage(object):
+class ImageGrid(object):
     def __init__(self, rows, cols, size, border_color=np.array([0.5, 0.5, 0.5])):
         self._rows = rows
         self._cols = cols
         self._size = size
         self._border = 1
-        self._border_color
+        self._border_color = np.array(border_color)
 
         self._fullsize = (self._border + (size[0] + self._border) * self._rows,
                           self._border + (size[1] + self._border) * self._cols)
 
         self._data = np.ones(self._fullsize + (3,), dtype=np.float64)
 
-    def set_image(self, row, col, image, vmin=None, vmax=None, cmap=None):
-        import matplotlib as plt
+    def set_image(self, image, row, col, vmin=None, vmax=None, cmap=None):
+        import matplotlib as mpl
+        import matplotlib.pylab as plt
+
         if cmap is None:
             cmap = plt.cm.gray
         if vmin is None:
@@ -26,10 +29,14 @@ class PlotImage(object):
 
         from gv.fast import resample_and_arrange_image
 
-        image_indices = (np.clip((image - vmin) / vmax, 0, 1) * 255).astype(np.uint8)
+        if vmin == vmax:
+            diff = 1
+        else:
+            diff = vmax - vmin
+            
+        image_indices = (np.clip((image - vmin) / diff, 0, 1) * 255).astype(np.uint8)
 
-        assert hasattr(cmap, 'lut_'), "Not a proper cmap given"
-        rgb = resample_and_arrange_image(image_indices, self._size, cmap)
+        rgb = resample_and_arrange_image(image_indices, self._size, mpl.colors.makeMappingArray(256, cmap))
 
         self._data[row * (self._size[0] + self._border) : (row + 1) * (self._size[0] + self._border) + self._border,
                    col * (self._size[1] + self._border) : (col + 1) * (self._size[1] + self._border) + self._border] = self._border_color 
