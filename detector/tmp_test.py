2bf45293564c321bfdc435b6f7cb09891ee5fe1f
Gustav Larsson <gustav.m.larsson@gmail.com>
2014-02-26 17:36:04 -0600

    Added image size to --modify-settings.

--- detector/tmp_test.py
+++ detector/tmp_test.py
@@ -2,11 +2,14 @@
 import numpy as np
 
 import gv
+import time
+import itertools
 
 def f(x, y):
+    time.sleep(1)
     return 2*y + x
 
-def starf(args):
+def f_star(args):
     return f(*args)
 
 N = 30
@@ -15,8 +18,6 @@ if gv.parallel.main(__name__):
     x = np.arange(N)
     y = 4 * np.arange(N)
     args = [(x[i], y[i]) for i in xrange(N)]
-    #for i, z in enumerate(gv.parallel.starmap(f, args)):
-    for i, z in enumerate(gv.parallel.imap(starf, args)):
-        print i, ':', 2*y[i] + x[i], z
-        assert 2*y[i] + x[i] == z
+    for z in gv.parallel.starmap(f, args):
+        print z
         
