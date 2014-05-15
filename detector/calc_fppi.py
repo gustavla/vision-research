943b2aaf9618ac2b729830029d4ef4c1d76cb8b8
Gustav Larsson <gustav.m.larsson@gmail.com>
2014-05-15 09:45:57 -0500

    Added calculate FPPI file.

---
+++ detector/calc_fppi.py
@@ -0,0 +1,32 @@
+from __future__ import division
+from __future__ import print_function
+import argparse
+
+parser = argparse.ArgumentParser(description='Test response of model')
+parser.add_argument('results', metavar='<file>', nargs='+', type=argparse.FileType('rb'), help='Filename of results file')
+
+args = parser.parse_args()
+results_files = args.results
+
+import numpy as np
+import gv
+
+use_other_style = False 
+
+tots = []
+for i, results_file in enumerate(results_files):
+    data = np.load(results_file)
+    
+    try:
+        num_images = data['num_images']
+    except:
+        num_images = 741
+    detections = data['detections']
+    tp_fn = int(data['tp_fn'])
+    fppi, miss_rate = gv.rescalc.calc_fppi_miss_rate(detections, tp_fn, num_images)
+
+    summary = gv.rescalc.calc_fppi_summary(fppi, miss_rate) 
+
+    tots.append(summary)
+
+print(tots, ',')
